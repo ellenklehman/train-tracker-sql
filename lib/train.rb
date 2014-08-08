8f5bd3f95085583f7c79cc31f3a5bcfe167cb4ab
Aoede Pando & Ellen Krause Lehman <student@epicodus.com>
2014-08-08 13:43:20 -0700

    adds a stop to a train

--- lib/train.rb
+++ lib/train.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Train
 attr_reader :name, :id
 
@@ -33,5 +34,25 @@ attr_reader :name, :id
   def edit(new_name)
     @name = new_name['name']
     DB.exec("UPDATE train_line SET name = '#{@name}';")
+  end
+
+  def stops
+    stops = []
+    results = DB.exec("SELECT * FROM train_stops WHERE train_line_id = #{self.id};")
+
+    results.each do |result|
+      station_id = result['train_station_id'].to_i
+      line_id = result['train_line_id'].to_i
+      id_search = DB.exec("SELECT * FROM train_station WHERE id = #{station_id};")
+      id_search.each do |result|
+        station_name = result['name']
+        stops << Station.new({'name' => station_name, 'id' => station_id})
+      end
+    end
+    stops
+  end
+
+  def add_stop(stop_id)
+    DB.exec("INSERT INTO train_stops (train_station_id, train_line_id) VALUES (#{stop_id}, #{self.id});")
   end
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -34,4 +34,19 @@ describe 'Train' do
     new_train.edit({'name' => 'red line'})
     expect(new_train.name).to eq('red line')
   end
+
+  it 'adds a stop to a train' do
+    new_train = Train.new({'name' => 'blue line'})
+    new_train.save
+    new_station = Station.new({'name' => 'Clackamas'})
+    new_station.save
+    new_train.add_stop(new_station.id)
+    expect(new_train.stops).to eq [new_station]
+  end
+
+  it 'begins with an empty array to store all the stops' do
+    new_train = Train.new({'name' => 'blue line'})
+    new_train.save
+    expect(new_train.stops).to eq []
+  end
 end
